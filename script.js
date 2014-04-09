0df0018da11f0edbedd9420039a30ffc2a900fc7
oboenikui <oboenikuitwitter@gmail.com>
2014-04-10 04:25:49 +0900

    use redirect

--- script.js
+++ script.js
@@ -1,11 +1,34 @@
-window.onload=function(){
-    document.getElementById("srchtxt").name="q";
-    document.getElementsByTagName("form")[0].action="http://www.google.com/search";
-    var scripts = document.getElementsByTagName("script");
-    for(var i=0;i<scripts.length;i++){
-	if(scripts[i].src.indexOf("/javascript/fp_base_bd_ga")!=-1){
-	    scripts[i].src="http://dl.dropboxusercontent.com/u/89996208/yahoo_de_google.js";
+var url = location.toString();
+var queries = url.split("?")[1].split("&");
+var getKeyWord = function(){
+    var keyWord;
+    for(var i=0;i<queries.length;i++){
+	if(queries[i].split("=")[0]=="p"){
+	    keyWord = queries[i].split("=")[1];
+	    break;
 	}
     }
-};
-
+    return keyWord;
+}
+var hosts = url.split("://")[1].split(".");
+if(hosts[1]=="shopping"){
+    location.replace("https://www.google.com/search?tbm=shop&q="+getKeyWord());
+} else {
+    switch(hosts[0]){
+    case "search":
+	location.replace("https://www.google.com/search?q="+getKeyWord());
+	break;
+    case "image":
+	location.replace("https://www.google.com/search?tbm=isch&q="+getKeyWord());
+	break;
+    case "video":
+	location.replace("https://www.google.com/search?tbm=vid&q="+getKeyWord());
+	break;
+    case "map":
+	location.replace("https://maps.google.co.jp/maps?q="+getKeyWord());
+	break;
+    case "news":
+	location.replace("https://www.google.com/search?tbm=nws&q="+getKeyWord());
+	break;
+    }
+}
